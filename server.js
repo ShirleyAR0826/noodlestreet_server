52bf00a8e21a9b291a5543efa0557b10cb71ca36
Shirley <shirley.apellido@gmail.com>
2023-03-23 10:59:42 +0800

    mongodb

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Define a schema for your data
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+});
+
+// Create a model based on the schema
+const Item = mongoose.model('Item', itemSchema);
+
+// Define a route to get all items
+app.get('/items', async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
